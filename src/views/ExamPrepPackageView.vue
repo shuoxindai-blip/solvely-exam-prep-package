7cc2c45a9c071f78426fe161cb417b4d5bb4d0c8
shuoxindai-blip <284842473+shuoxindai-blip@users.noreply.github.com>
2026-09-14 14:05:34 +0800

    Make upload illustration labels responsive

--- src/views/ExamPrepPackageView.vue
+++ src/views/ExamPrepPackageView.vue
@@ -3124,8 +3124,8 @@ onBeforeUnmount(() => {
                           <svg><use href="#i-study-guide-spark" /></svg>
                         </span>
                         <span class="generator-result-copy">
-                      <small>FOCUSED</small>
-                      <strong>Priority topics</strong>
+                          <small>FOCUSED</small>
+                          <strong>Priority topics</strong>
                         </span>
                         <span class="generator-result-check">✓</span>
                       </span>
